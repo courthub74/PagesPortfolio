255e73baeccea5052e2f9371479ec87ebfa4c2f3
Courdevelops <courdevelops@gmail.com>
2023-04-24 19:27:53 -0400

    blog scroll started

---
+++ blogscroll.js
@@ -0,0 +1,23 @@
+// Query through the blog circles
+
+// The Blog Circles
+const blogCircles = document.querySelectorAll(".blogcircles") ;
+console.log("BLOGCIRCLES === ", blogCircles);
+
+// The Left Scroll Arrow
+const scrollThrough = document.querySelector("#scrollthrough");
+console.log("SCROLLTHROUGH === ", scrollThrough);
+
+// The Right Scroll Arrow
+const scrollBack = document.querySelector("#scrollback");
+console.log("SCROLLBACK === ", scrollBack);
+
+
+// Iterate through each blog circle
+blogCircles.forEach((item, i) => {
+    let containerBlogDimensions = item.getBoundingClientRect();
+    let containerWidth = containerBlogDimensions.width;
+
+    // add event listeners
+    
+})
